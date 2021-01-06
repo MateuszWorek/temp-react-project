82d9bedcc52fdf73ebf7dbafab7e31051e29b3b6
Hyperion1040 <mateusz.worek@gmail.com>
2021-01-06 13:10:51 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt laboriosam animi dolor esse hic, ducimus dignissimos excepturi mollitia quo dolorem molestiae aliquam cumque voluptates! Accusantium rerum unde laborum mollitia sint iure. Deserunt quo, pariatur nobis cupiditate aliquid ratione minima error numquam a corporis et. Voluptatum, iure? Dolorem odio consequatur fuga.</p>
+        </section>
+    );
+}
